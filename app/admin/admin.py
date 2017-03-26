ec2b5928f378efb7c4734c2d53b6fa190b9487c2
73spica <harustar095@gmail.com>
2017-03-26 15:51:21 +0900

    admin用のログイン処理追加．

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -11,16 +11,49 @@ app = Blueprint('admin',__name__, url_prefix="/admin",template_folder='../admin/
 
 @app.route("/")
 @app.route("/home")
-def admin():
+def home():
     return render_template("home_for_admin.html")
 
-@app.route("/login")
+@app.route('/login',methods=['GET','POST'])
 def login():
-    return render_template("login_for_admin.html")
+    error = None
+    if request.method=='POST':
+        #if valid_login(request.form['username'],request.form['password']):
+        success,msg = doLogin(request.form['username'],request.form['password'])
+        if success:
+            session['username'] = request.form['username']
+            session['logged_in'] = True
+            return redirect(url_for('admin.home'))
+        else:
+            error = msg
+        return render_template('login_for_admin.html',error=error)
+    else:
+        return render_template('login_for_admin.html')
+
+@app.route('/logout',methods=['GET'])
 
 @app.route("/problem")
 def problem():
     return render_template("problem_for_admin.html")
+
+
+def doLogin(username,password):
+    try:
+        with db.transaction():
+            pass_hash = hashlib.md5(password.encode('utf-8')).hexdigest()
+            user = Users.get(Users.username==username and Users.password==pass_hash)
+    except (IntegrityError,Users.DoesNotExist) as ex:
+        print (ex)
+        db.rollback()
+        message = "username/password is wrong."
+        return (False,message)
+    else:
+        if user:
+            message = "Login Success!"
+            return (True,message)
+        else:
+            message = "username/password is wrong."
+            return (False,message)
 
 @app.route('/register',methods=['POST'])
 def register():
