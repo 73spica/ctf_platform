16d0690e36a862335d1a8e2bca818bcf51e31cbf
73spica <harustar095@gmail.com>
2017-03-30 23:34:00 +0900

    adminの問題追加につかうテーブルの追加

--- app/admin/model/model.py
+++ app/admin/model/model.py
@@ -1,6 +1,7 @@
 from peewee import *
 
-db = SqliteDatabase('for_admin.db')
+db_for_admin = SqliteDatabase('for_admin.db')
+db = SqliteDatabase('m1z0r3ctf.db')
 
 class Users(Model):
     username = CharField()
@@ -12,6 +13,19 @@ class Users(Model):
     solved = CharField()
 
     class Meta:
+        database = db_for_admin
+
+db_for_admin.create_tables([Users],True)
+
+class Problems(Model):
+    name = CharField()
+    point = IntegerField()
+    genre = CharField()
+    flag = CharField()
+    detail = CharField()
+    author = CharField()
+
+    class Meta:
         database = db
 
-db.create_tables([Users],True)
+db.create_tables([Problems],True)
