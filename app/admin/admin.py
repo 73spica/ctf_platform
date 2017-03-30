4cc7d30869909c0b576d954e701df80e1772450e
73spica <harustar095@gmail.com>
2017-03-30 23:33:29 +0900

    問題の追加処理とデータベースの追加・修正

--- app/admin/admin.py
+++ app/admin/admin.py
@@ -5,7 +5,7 @@ from peewee import *
 from datetime import date
 import json
 import hashlib
-from .model.model import Users, db
+from .model.model import Users, db_for_admin, Problems, db
 
 app = Blueprint('admin',__name__, url_prefix="/admin",template_folder='../admin/templates')
 
@@ -43,18 +43,37 @@ def problem():
 @app.route("/adding_problems",methods=['GET','POST'])
 def adding_problems():
     if request.method=='POST':
-        return redirect(url_for("admin.home"))
+        success = addProblem(request.form['title'], request.form['point'], request.form['genre'], request.form['flag'], request.form['detail'], request.form['author'])
+        if success:
+            message = "Successfully added the problem."
+            return render_template('adding_problems.html',success=message)
+        else:
+            error = "Failed to add problem. Please again."
+            return render_template('adding_problems.html',error=message)
     else:
         return render_template("adding_problems.html")
 
-def doLogin(username,password):
+def addProblem(title, point, genre, flag, detail, author):
     try:
         with db.transaction():
+            # フラグもハッシュ取る
+            flag = hashlib.md5(flag.encode("utf-8")).hexdigest()
+            problem = Problems(name=title, point=point, genre=genre, flag=flag, author=author, detail=detail)
+            problem.save()
+        return True
+    except IntegrityError as ex:
+        print (ex)
+        db.rollback()
+        return False
+
+def doLogin(username,password):
+    try:
+        with db_for_admin.transaction():
             pass_hash = hashlib.md5(password.encode('utf-8')).hexdigest()
             user = Users.get(Users.username==username and Users.password==pass_hash)
     except (IntegrityError,Users.DoesNotExist) as ex:
         print (ex)
-        db.rollback()
+        db_for_admin.rollback()
         message = "username/password is wrong."
         return (False,message)
     else:
@@ -76,14 +95,14 @@ def register():
 
 def doRegister(username,password):
     try:
-        with db.transaction():
+        with db_for_admin.transaction():
             pass_hash = hashlib.md5(password.encode("utf-8")).hexdigest()
             user = Users(username=username, password=pass_hash, created_at=date.today(), is_admin=True, is_active=True, score=0, solved=json.dumps([]))
             user.save()
         return True
     except IntegrityError as ex:
         print (ex)
-        db.rollback()
+        db_for_admin.rollback()
         return False
 
 # TODO: セッションに入れるキーはどこかにまとめておくべき？
